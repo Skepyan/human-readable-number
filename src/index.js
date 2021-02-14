9fa5de6e3279443a4ddcb7d7a78168c61e4bb183
unknown <you@example.com>
2021-02-14 15:21:52 +0300

    feat: add function human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function toReadable (number) {
-  
+module.exports = 
+  function toReadable (number) {
+    let hundred = Math.floor(number / 100);
+    let tens = Math.floor((number - hundred * 100 ) / 10);
+    let numeric = Math.floor( number - hundred * 100 - tens *10);
+
+    let ArrayNumeric = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+    'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 
+       'seventeen', 'eighteen', 'nineteen'];
+   let ArrayTens = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ',
+       'eighty ', 'ninety '];
+   let hundredHuman = ArrayNumeric[hundred] + ' hundred ';
+   let hundredHuman2 = ArrayNumeric[hundred];
+
+  if (number === 0){
+      return 'zero';
+  }else if (tens === 1 && number.toString().length > 2){
+    tens = 0;
+    numeric = number - hundred * 100
+    return (hundredHuman + ArrayNumeric[numeric])
+  }else if (tens === 1 && number.toString().length <= 2){
+    return  ArrayNumeric[numeric + 10]
+  }
+  else{
+    if ( hundred >= 1){
+      hundredHuman = hundredHuman;
+    }else{
+      hundredHuman = ''; 
+    }
+    let xxx = (hundredHuman + ArrayTens[tens]  + ArrayNumeric[numeric]).split(' ')
+    console.log (xxx);
+    let rrr = [];
+    for( let i =0; i < xxx.length; i++){
+      if(xxx[i].length > 0)
+      rrr.push(xxx[i])
+    }
+    console.log(rrr);
+return rrr.join(' ').trim();
+}
+
 }
